d6f696b885dc356afdf1325bd2f78e8ef6a0c666
isaiahnoelpulidosalazar <isaiahnoelpulidosalazar@gmail.com>
2026-06-06 17:48:29 +0800

    add main system

---
+++ chease.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+
+// current version - 1.0.0
+
+// changes:
+// - add main system
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: ./chease.exe filename.chease");
+            return;
+        }
+    }
+}
